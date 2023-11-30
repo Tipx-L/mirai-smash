fa1625cc7074318760053ce93d6ffcf7f288b35e
Tipx-L <138244655+Tipx-L@users.noreply.github.com>
2023-11-30 00:30:41 -0800

    Make Arena.kt serializable.

--- src/main/kotlin/smash/Arena.kt
+++ src/main/kotlin/smash/Arena.kt
@@ -2,8 +2,10 @@ package io.github.tipx_l.miraismash.smash
 
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
 import net.mamoe.mirai.console.command.CommandContext
 
+@Serializable
 data class Arena(
 	val context: CommandContext,
 	val arenaID: String,
