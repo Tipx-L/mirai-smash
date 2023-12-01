96aabc90d39cb9332c742faf8a2be53b2e37235a
Tipx-L <138244655+Tipx-L@users.noreply.github.com>
2023-12-01 08:09:46 -0800

    Test sending images: 2.

--- src/main/kotlin/command/TestCommand.kt
+++ src/main/kotlin/command/TestCommand.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.withContext
 import net.mamoe.mirai.console.command.CommandContext
 import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.contact.Contact.Companion.sendImage
+import java.awt.Font
 import java.awt.image.BufferedImage
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
@@ -17,7 +18,10 @@ object TestCommand : SimpleCommand(MiraiSmash, "测试", description = "测试")
 	suspend fun test(context: CommandContext) {
 		val bufferedImage = BufferedImage(1280, 720, BufferedImage.TYPE_INT_RGB)
 		val graphics = bufferedImage.createGraphics()
-		graphics.drawString(MiraiSmashData.arenas.joinToString("\n"), 0F, 360F)
+		graphics.font = Font("SansSerif", Font.PLAIN, 64)
+		MiraiSmashData.arenas.forEachIndexed { index, arena ->
+			graphics.drawString(arena.toString(), 0, 64 * (index + 1))
+		}
 		val byteArrayOutputStream = ByteArrayOutputStream()
 		withContext(Dispatchers.IO) {
 			ImageIO.write(bufferedImage, "png", byteArrayOutputStream)
